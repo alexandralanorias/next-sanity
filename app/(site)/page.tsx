09b658b411b516e614add45bdead1a2f9ca73562
alexandralanorias <97576137+alexandralanorias@users.noreply.github.com>
2023-05-18 15:43:05 +0800

    Update page.tsx

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -10,7 +10,7 @@ export default async function Home() {
       <h1 className="text-7xl font-extrabold text-zinc-400">hi hello i&apos;m
         <span className="bg-gradient-to-r from-orange-300 via-orange-100 to-amber-200 bg-clip-text text-transparent"> alexa!</span>
       </h1>
-      <p className="mt-3 text-xl text-amber-100">I'm a Filipina developer from Davao City, and I love tinkering with React and Next.js!</p>
+      <p className="mt-3 text-xl text-amber-100">I&apos;m a Filipina developer from Davao City, and I love tinkering with React and Next.js!</p>
       <h2 className="mt-24 font-bold text-amber-100 text-3xl">My Projects</h2>
 
       <div className="mt-5 grid md:grid-cols-2 lg:grid-cols-3 gap-8">{projects.map((project) => (
